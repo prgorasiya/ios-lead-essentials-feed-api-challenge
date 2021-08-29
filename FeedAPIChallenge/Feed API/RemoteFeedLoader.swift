a9700e4951574c0b3ca123f770614566f78c9e29
Paras <prgorasiya@yahoo.com>
2021-08-29 12:19:19 +0800

    delivers connectivity error on client error

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,6 +19,13 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		fatalError("Must be implemented")
+		client.get(from: url) { result in
+			switch result {
+			case .success((_, _)):
+				completion(.failure(RemoteFeedLoader.Error.invalidData))
+			case .failure(_):
+				completion(.failure(RemoteFeedLoader.Error.connectivity))
+			}
+		}
 	}
 }
